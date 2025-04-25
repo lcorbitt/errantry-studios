e26a848595372a38bbe9ace72613491951e82321
Lukas Corbitt <movalley5@gmail.com>
2025-04-25 17:15:27 -0600

    Linting

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ const ContactPage = () => {
     <div className="min-h-screen bg-black text-white">
       <h2 className="text-3xl text-center py-8 font-bold">Contact Us</h2>
       <p className="text-center text-white/80 max-w-2xl mx-auto mb-8">
-        Got a story to tell or an idea to create? Let's make something great together. <br /> Fill out the form below and we'll be in touch soon.
+        Got a story to tell or an idea to create? Let&apos;s make something great together. <br /> Fill out the form below and we&apos;ll be in touch soon.
       </p>
       <ContactForm />
     </div>
